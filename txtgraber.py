3d3a79170d291c08d2685a079967f894f536776d
K <23080326+AhmedKAwwad@users.noreply.github.com>
2022-08-12 14:28:22 +0200

    Update txtgraber.py

--- txtgraber.py
+++ txtgraber.py
@@ -3,21 +3,7 @@ from termcolor import colored
 from os import listdir
 from os.path import isfile, join 
 arguments = len(sys.argv)
-if arguments > 3:
-  print (colored("Ops! Too Much inputs","red")) 
-  print (colored("==========================================","red"))   
-  print (colored("""
-    _______  ________   ______           __             
-   /_  __/ |/ /_  __/  / ____/________ _/ /_  ___  _____
-    / /  |   / / /    / / __/ ___/ __ `/ __ \/ _ \/ ___/
-   / /  /   | / /    / /_/ / /  / /_/ / /_/ /  __/ /    
-  /_/  /_/|_|/_/     \____/_/   \__,_/_.___/\___/_/     
-                                                        
-  ""","blue"))
-  print (colored("==========================================","red"))
-  print (colored("A small guide for TXT Graber","green"))
-  print (colored("python txtgraber.py DirPath OutPutFile.txt","green"))
-elif arguments == 2 or sys.argv[1] == "help":
+def banner():
   print (colored("""
     _______  ________   ______           __             
    /_  __/ |/ /_  __/  / ____/________ _/ /_  ___  _____
@@ -29,6 +15,14 @@ elif arguments == 2 or sys.argv[1] == "help":
   print (colored("==========================================","yellow"))
   print (colored("A small guide for TXT Graber","green"))
   print (colored("python txtgraber.py DirPath OutPutFile.txt","green"))
+  print (colored("==========================================","yellow"))
+
+if arguments > 3:
+  print (colored("Ops! Too Much inputs","red")) 
+  print (colored("==========================================","red"))   
+  banner()
+elif arguments == 2 or sys.argv[1] == "help":
+  banner()
   
 else:
   folder_path = sys.argv[1]
@@ -46,15 +40,7 @@ else:
           if not (line.startswith("#") or line.startswith(" ")):
               f.write(line)   
   f.close()
-  print (colored("""
-    _______  ________   ______           __             
-   /_  __/ |/ /_  __/  / ____/________ _/ /_  ___  _____
-    / /  |   / / /    / / __/ ___/ __ `/ __ \/ _ \/ ___/
-   / /  /   | / /    / /_/ / /  / /_/ / /_/ /  __/ /    
-  /_/  /_/|_|/_/     \____/_/   \__,_/_.___/\___/_/     
-                                                        
-  ""","blue"))
-  print (colored("==========================================","yellow"))
+  banner()
   print (colored("Grabed all Text in .txt files in the Directory","green"),folder_path,colored("is completed","green"))
   print (colored("You can find them in","green"),outputFile)
   # print (colored("python txtgraber.py DirPath OutPutFile.txt","green"))
